2c0b1508212eb68c78ee737efd41745797e65cc0
IamBearBG <iambearbg@gmail.com>
2025-01-05 13:19:29 -0500

    Add files via upload

---
+++ puglins/descargas-imagen.js
@@ -0,0 +1,16 @@
+import {googleImage} from '@bochilteam/scraper';
+const handler = async (m, {conn, text, usedPrefix, command}) => {
+if (!text) return conn.reply(m.chat, '✧ Ingresa el texto de lo que quieres buscar', m, rcanal);
+await m.react(rwait)
+const res = await googleImage(text);
+const image = await res.getRandom();
+const link = image;
+const messages = [['Imagen 1', dev, await res.getRandom(),
+[[]], [[]], [[]], [[]]], ['Imagen 2', dev, await res.getRandom(), [[]], [[]], [[]], [[]]], ['Imagen 3', dev, await res.getRandom(), [[]], [[]], [[]], [[]]], ['Imagen 4', dev, await res.getRandom(), [[]], [[]], [[]], [[]]]]
+await conn.sendCarousel(m.chat, `✧ Resultado de ${text}`, '🔎 Imagen - Descargas', null, messages, m);
+};
+handler.help = ['imagen *<texto>*'];
+handler.tags = ['search', 'dl'];
+handler.command = ['image','imagen'];
+handler.register = true
+export default handler;
---
+++ puglins/descargas-pinterest.js
@@ -0,0 +1,41 @@
+/* 
+
+[ Canal Principal ] :
+https://whatsapp.com/channel/0029Vb2NkWWFsn0ghn9mOA2G
+*/
+
+// *[ ✧ PINTEREST DL (Imagen/video) ]*
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+if (!text) {
+return conn.reply(m.chat, '✧ Ingresa el link de un video/imagen de pinterest', m)
+}
+
+try {
+let api = await fetch(`https://api.giftedtech.my.id/api/download/pinterestdl?apikey=gifted&url=${text}`)
+let res = await api.json()
+let json = res.result
+
+if (Array.isArray(json.media)) {
+for (let media of json.media) {
+if (media.format === "JPG") {
+await conn.sendFile(m.chat, media.download_url, 'HasumiBotFreeCodes.jpg', `✰ ${json.title}`, m)
+} else if (media.format === "MP4") {
+await conn.sendFile(m.chat, media.download_url, 'HasumiBotFreeCodes.mp4', `✰ ${json.title}`, m)
+}}
+} else {
+let media = json.media
+if (media.format === "JPG") {
+await conn.sendFile(m.chat, media.download_url, 'HasumiBotFreeCodes.jpg', `✰ ${json.title}`, m)
+} else if (media.format === "MP4") {
+await conn.sendFile(m.chat, media.download_url, 'HasumiBotFreeCodes.mp4', `✰ ${json.title}`, m)
+}}
+
+} catch (error) {
+console.error(error)
+}}
+
+handler.command = /^(pinterestdl)$/i
+
+export default handler
