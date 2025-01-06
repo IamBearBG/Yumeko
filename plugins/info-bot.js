08629deac0aed2392139908c3070f4a74d3e18d7
IamBearBG <iambearbg@gmail.com>
2025-01-05 19:46:13 -0500

    Add files via upload

---
+++ plugins/info-bot.js
@@ -0,0 +1,25 @@
+import fs from 'fs';
+const handler = (m) => m;
+handler.all = async function(m) {
+  const vn = './media/audios/bot.mp3';
+  const chat = global.db.data.chats[m.chat];
+
+  if (/^bot$/i.test(m.text) && !chat.isBanned) {
+    conn.sendPresenceUpdate('recording', m.chat);
+conn.reply(m.chat, `✧ ¡Hola! Soy Yumeko, en que puedo ayudarte hoy?\n\n✰ Usa *.menu* para ver mis comandos.`, m, rcanal, )
+}
+if (/^sexo$/i.test(m.text)) {
+conn.reply(m.chat, `*pervertido* 🫣`, m, rcanal, )
+}
+if (/^tetas|teta$/i.test(m.text)) {
+conn.reply(m.chat, `*que caliente eres* 🥵`, m, rcanal, )
+}
+if (/^bug$/i.test(m.text)) {
+conn.reply(m.chat, `*tu mamá we* 😹`, m, rcanal, )
+}
+if (/^pene$/i.test(m.text)) {
+conn.reply(m.chat, `*comes* 😹`, m, rcanal, )
+}
+  return !0;
+};
+export default handler;
