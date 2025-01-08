34a44dae830cd4388508e3ba832f5607a08c80ee
IamBearBG <iambearbg@gmail.com>
2025-01-08 10:26:07 -0500

    Add files via upload

---
+++ plugins/info-grupos.js
@@ -0,0 +1,25 @@
+import fetch from 'node-fetch'
+
+let handler  = async (m, { conn, usedPrefix, command }) => {
+let img = await (await fetch(`https://i.ibb.co/rtLQsxW/file.jpg`)).buffer()
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)
+let txt = `*Hola!, te invito a unirte a los grupos oficiales de del Bot para convivir con la comunidad :D*
+
+1- Grupo Yumeko
+*✧* https://chat.whatsapp.com/CCHrnw3MCTxKDEMPAVeDJs
+
+*─ׄ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׅ─ׄ⭒─ׄ─ׄ*
+
+➠ Enlace anulado? entre aquí! 
+
+Canal Yumeko Bot :
+*✧* https://whatsapp.com/channel/0029VaJxgcB0bIdvuOwKTM2Y
+
+> ${dev}`
+await conn.sendFile(m.chat, img, "Thumbnail.jpg", txt, m, null, fake)
+}
+handler.help = ['grupos']
+handler.tags = ['main']
+handler.command = /^(grupos)$/i
+export default handler
